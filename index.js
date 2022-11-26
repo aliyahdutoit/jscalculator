d6fda24ad9f457151fe4c1e7a7658e66208eb5fa
aliyahdutoit <aliyah.dutoit@gmail.com>
2022-11-26 14:12:20 +0200

    update 8

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const output = document.getElementById("screen");
-const calDisplay = document.getElementById("Display");
-function display(num){
-    output.value += num;
+const decimal = document.getElementById("decimal").value;
+
+function display(num) {
+  output.value += num;
+  if (decimal === ".") {
+    document.getElementById("decimal").disabled = false;
+  } else {
+    document.getElementById("decimal").disabled = true;
+  }
 }
-function allClear(){
-    output.value = '';
-    output.value = '';
+function allClear() {
+  output.value = "";
+  output.value = "";
 }
-function deletespace(){
-    output.value = output.value.slice(0,-1);
+function deletespace() {
+  output.value = output.value.slice(0, -1);
 }
-function calculate(){
-    output.value = eval(output.value);
+function calculate() {
+  output.value = eval(output.value);
 }
 
 function clearspace() {
-    document.getElementById("screen").value = "";
+  document.getElementById("screen").value = "";
 }
-
-
-
-
-
-
